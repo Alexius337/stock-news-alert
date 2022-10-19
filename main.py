b46ce9c306186cfaa0335598b0bf118d91179f84
Alexius337 <almyro@gmail.com>
2022-10-18 21:33:11 -0700

    finalized script and verified behavior

--- main.py
+++ main.py
@@ -13,15 +13,14 @@ auth_token  = config.twilio_api_token
 twilio_number = config.my_twilio_number
 outgoing_number = config.outgoing_number
 
-# Today's and yesterday's dates:
+# Today's date:
 date_today = time.strftime("%Y-%m-%d")
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
+# Stock API endpoint and auth
 stock_api_endpoint = "https://www.alphavantage.co/query?"
 stock_api_key = config.stock_price_api_key
 
+# Stock API params
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
@@ -30,58 +29,51 @@ stock_params = {
     "apikey": stock_api_key,
 }
 
+# Send request to stock API
 stock_response = requests.get(stock_api_endpoint, params=stock_params)
 stock_response.raise_for_status()
 
+# Process stock API response data
 stock_data = stock_response.json()
 stock_today = stock_data["Time Series (Daily)"][date_today]
 stock_open = float(stock_today["1. open"])
 stock_close = float(stock_today["4. close"])
 
-acceptable_daily_change = float(stock_open) / 100
+acceptable_daily_change = stock_open / 100
 stock_change = stock_close - stock_open
 stock_message = ""
 
+# Custom message for weather stock increased or decreased
 if stock_open > stock_close + acceptable_daily_change:
     stock_message += STOCK +": " + "🔻" + " " + str(round(stock_change, 2)) + "%\n" + "\n"
-    #print(f"The price of {STOCK} {COMPANY_NAME} has dropped significantly.")
+
 elif stock_open < stock_close + acceptable_daily_change:
     stock_message += STOCK +": " + "🔺" + " " + str(round(stock_change, 2)) + "%\n" + "\n"
-    #print(f"The price of {STOCK} {COMPANY_NAME} has risen significantly.")
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
+# News API auth
 news_api_key = config.news_api_key
 
+# Send request to news API
 newsapi = NewsApiClient(api_key=news_api_key)
 all_articles = newsapi.get_everything(q=COMPANY_NAME, from_param=date_today, to=date_today, language='en')
 
+# Add 3 most recent news articles for our stock to the message
 for article in all_articles["articles"][0:3]:
     stock_message =  stock_message + article["title"] + "\n"
     stock_message =  stock_message + article["url"] + "\n" + "\n"
 
+# Verify the message locally that we plan to send
 print(stock_message)
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
+# Twilio SMS auth
 client = Client(account_sid, auth_token)
+
+# Twilio send SMS with our message
 message = client.messages.create(
     to=outgoing_number, 
     from_=twilio_number,
     body=stock_message
     )
+
+# Check message status
 print(message.status)
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
